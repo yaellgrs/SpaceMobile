2fe8b9a19d31c8902c990db60b63e33728b57ad4
yaellgrs <yaellegris09@gmail.com>
2026-01-21 18:22:46 +0100

    Ship upgrade Element :  Barre de progression fonctionelle

--- Assets/Scripts/UI/prestige/shipUpgradeElement.cs
+++ Assets/Scripts/UI/prestige/shipUpgradeElement.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,8 +9,24 @@ public partial class shipUpgradeElement : VisualElement
     Label Lbl_price;
     VisualElement VE_logoPrice;
     VisualElement VE_logo;
+    VisualElement VE_progressCadre;
+    VisualElement VE_progressBar;
+    VisualElement VE_progressBarHidder;
 
     Button Btn_buy;
+
+    public int _level;
+
+    [UxmlAttribute]
+    public int Level
+    {
+        get => _level;
+        set
+        {
+            _level = Mathf.Clamp(value, 0, 5);
+            SetShipLevel(_level);
+        }
+    }
 
     public shipUpgradeElement()
     {
@@ -18,24 +35,42 @@ public partial class shipUpgradeElement : VisualElement
         Lbl_name = new Label();
         VE_logoPrice = new VisualElement();
         VE_logo = new VisualElement();
+        VE_progressCadre = new VisualElement();
+        VE_progressBar = new VisualElement();
+        VE_progressBarHidder = new VisualElement();
         Lbl_price = new Label();
         Btn_buy = new Button();
 
         Lbl_name.AddToClassList("ShipName");
         VE_logoPrice.AddToClassList("ShipLogoPrice");
         VE_logo.AddToClassList("ShipLogo");
+        VE_progressCadre.AddToClassList("ShipProgressCadre");
+        VE_progressBar.AddToClassList("ShipProgressBar");
         Lbl_price.AddToClassList("ShipPrice");
         Btn_buy.AddToClassList("ShipBuy");
 
         Lbl_name.text = "Basic SpaceShip";
-        Btn_buy.text = "buy";
+        Btn_buy.text = "UP";
         Lbl_price.text = "0/10";
 
         Add( Lbl_name );
         Add(VE_logo);
-        Lbl_price.Add(VE_logoPrice);
         Add(Lbl_price);
         Add(Btn_buy);
+        Add(VE_progressBar);
+        VE_progressBar.Add(VE_progressCadre);
+        Lbl_price.Add(VE_logoPrice);
+        
     }
+
+    public void SetShipLevel(int level)
+    {
+        string path = "ship/progresBarShipLevel" + level;
+        Texture2D tex = Resources.Load<Texture2D>(path);
+        VE_progressBar.style.backgroundImage = new StyleBackground(tex);
+
+
+    }
+ 
 
 }
